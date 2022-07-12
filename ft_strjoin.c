f0f27ab6a7272a505ae9ef3c45b14f8b60500607
Kubra Beceren <kbeceren@student.s19.be>
2022-07-12 11:29:36 +0200

    add changes

---
+++ ft_strjoin.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbeceren <kbeceren@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/12 10:32:43 by kbeceren          #+#    #+#             */
+/*   Updated: 2022/07/12 11:28:55 by kbeceren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+*	DESCRIPTION
+*	Allocates (with malloc(3)) and returns a new string, which is the result of 
+*	the concatenation of ’s1’ and ’s2’.
+*	PARAMETERS
+*	#1. The prefix string.
+*	#2. The suffix string.
+*	RETURN VALUES
+*	The new string. NULL if the allocation fails.
+*/
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char			*s3;
+	unsigned int	i;
+	unsigned int	j;
+
+	if (!s1 || !s2)
+		return (0);
+	s3 = (char *)malloc(sizeof(*s3) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!s3)
+		return (0);
+	i = 0;
+	j = 0;
+	while (s1[i] != 0)
+	{
+		s3[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != 0)
+	{
+		s3[i] = s2[j];
+		i++;
+		j++;
+	}
+	s3[i] = 0;
+	return (s3);
+}
--- ft_substr.c
+++ ft_substr.c
@@ -6,10 +6,21 @@
 /*   By: kbeceren <kbeceren@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/11 15:42:40 by kbeceren          #+#    #+#             */
-/*   Updated: 2022/07/12 10:31:31 by kbeceren         ###   ########.fr       */
+/*   Updated: 2022/07/12 11:17:51 by kbeceren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+*	DESCRIPTION
+*	Allocates (with malloc(3)) and returns a substring from the string ’s’.
+*	The substring begins at index ’start’ and is of maximum size ’len’.
+*	PARAMETERS
+*	#1. The string from which to create the substring.
+*	#2. The start index of the substring in the string ’s’.
+*	#3. The maximum length of the substring.
+*	RETURN VALUES
+*	The substring. NULL if the allocation fails.
+*/
 #include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)

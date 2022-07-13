ec861bec38ad1fe9fea1e25eedcd204f6a36c1ee
Kubra Beceren <kbeceren@student.s19.be>
2022-07-13 13:08:49 +0200

    add

---
+++ ft_striteri.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbeceren <kbeceren@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/13 12:02:46 by kbeceren          #+#    #+#             */
+/*   Updated: 2022/07/13 12:05:33 by kbeceren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+*	DESCRIPTION
+*	Applies the function f to each character of the string passed as argument, 
+*	and passing its index as first argument. Each character is passed by
+*	address to f to be modified if necessary.
+*	PARAMETERS
+*	#1. The string on which to iterate.
+*	#2. The function to apply to each character.
+*	RETURN VALUES
+*	-
+*/
+
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	unsigned int	i;
+
+	if (s != 0 && f != 0)
+	{
+		i = 0;
+		while (s[i])
+		{
+			f(i, s + i);
+			i++;
+		}
+	}
+}
---
+++ ft_strmapi.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbeceren <kbeceren@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/13 12:03:31 by kbeceren          #+#    #+#             */
+/*   Updated: 2022/07/13 13:08:32 by kbeceren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+*	DESCRIPTION
+* 	Applies the function ’f’ to each character of the ’s’ to create a new 
+*	string (with malloc) resulting from successive applications of ’f’.
+*	PARAMETERS
+*	#1. The string on which to iterate.
+*	#2. The function to apply to each character.
+*	RETURN VALUES
+*	The string created from the successive applications of ’f’. 
+*	Returns NULL if the allocation fails.
+*/
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*str;
+	int		len;
+	int		i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	len = ft_strlen(s);
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (0);
+	while (i < len)
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[len] = '\0';
+	return (str);
+}
